deaa31ef29946dc0a2f85ec269873df262bcf0a6
Jorge Armando Ceras Cárdenas <jorge.armando.c.cardenas@gmail.com>
2025-08-29 12:52:16 -0600

    Optimización de estilos e imágenes

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,6 +1,7 @@
-
+// Configuración de Supabase
 const SUPABASE_URL = 'https://supabase.com/dashboard/project/wckpsfmlhzabubbjncxc/editor/17269?schema=public';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indja3BzZm1saHphYnViYmpuY3hjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ0MjM5MDEsImV4cCI6MjA2OTk5OTkwMX0.EpA7kwuviqGzvUdJ6j1NbKBFBE-ureqwL_9HMWRuxU4;'
 
-
+// Inicializar cliente de Supabase
+// Cambiamos esta línea para usar el objeto global importado desde el CDN
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
